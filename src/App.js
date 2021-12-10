d38c40d8c045f3608fd91dc44295af48dda08752
Brandyn <stateofw@gmail.com>
2021-12-10 13:52:44 -0500

    mvp completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
-
+import { fetchSmurfs } from "./actions";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
-const App = ()=> {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchSmurfs();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
 
-      <main>
-        <SmurfList/>
-        <AddForm/>
-      </main>
-    </div>
-  );
+        <main>
+          <SmurfList/>
+          <AddForm/>
+        </main>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, {fetchSmurfs})(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
-//2. Call the fetchSmurfs action when the component mounts.
+//2. Call the fetchSmurfs action when the component first loads.
--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { setError, addSmurf } from './../actions';
 
 const AddForm = (props) => {
     const [state, setState] = useState({
@@ -7,10 +9,6 @@ const AddForm = (props) => {
         nickname:"",
         description:""
     });
-
-    //remove when error state is added
-    const errorMessage = "";
-
     const handleChange = e => {
         setState({
             ...state,
@@ -20,10 +18,9 @@ const AddForm = (props) => {
 
     const handleSubmit = e => {
         e.preventDefault();
+        props.addSmurf(state);
         if (state.name === "" || state.position === "" || state.nickname === "") {
-            //dispatch a custom error action
-        } else {
-            //dispatch an addSmurf action
+            props.setError("Name, position and nickname fields are required.");
         }
     }
 
@@ -47,15 +44,23 @@ const AddForm = (props) => {
                 <textarea onChange={handleChange} value={state.description} name="description" id="description" />
             </div>
             {
-                errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {errorMessage}</div>
-            }
-            <button>Submit Smurf</button>
-        </form>
-    </section>);
-}
-
-export default AddForm;
-
+                 props.errorMessage && <div data-testid="errorAlert" className="alert alert-danger" role="alert">Error: {props.errorMessage}</div>
+                }
+                <button>Submit Smurf</button>
+            </form>
+        </section>);
+    }
+    const mapStateToProps = state => {
+        return {
+            errorMessage: state.error
+        }
+    }
+    
+    const mapActionsToProps = {
+        setError, 
+        addSmurf 
+    }
+    export default connect (mapStateToProps, mapActionsToProps)(AddForm)
 //Task List:
 //1. Connect the errorMessage, setError and addSmurf actions to the AddForm component.
 //2. Replace all instances of the errorMessage static variable with your error message state value. 
